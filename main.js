e8692ddc6ee9e5883ae165d06fd187432ff456ba
mhant <michael@androiddev101.com>
2020-06-04 20:38:30 +0300

    adding functionf for relative to and turndirection

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var gController;
 var introController;
 
-function intro(){
+function intro() {
     introController = new HowToController(startGame);
 }
 
@@ -13,11 +13,11 @@ function startGame() {
     }
     let playerPlace = { "x": 100, "y": 100 };
     let enemyTypesPlaces = randomEnemies();
-    let gatePlace = { "x": (getWidth() - 100), "y": ((getHeight() - 75) / 2) }
+    let gatePlace = { "x": (getWidth() - 100), "y": ((getHeight() - 75) / 2) };
     gController = new GameController(playerPlace, enemyTypesPlaces, gatePlace, finishCallback);
     window.onresize = function (event) {
         // if not already ended, end game
-        if (!gController.gameOver){
+        if (!gController.gameOver) {
             gController.end();
             alert("Do not resize during game, please refresh.");
         }
@@ -44,7 +44,9 @@ function randomEnemies() {
     var count = Math.floor(Math.random() * 4) + 3;
     // max random count of enemis for screen
     count = Math.min(count, maxEnemies);
-    for (var i = 0; i < count; i++) {
+    enemyTypesPlaces.push(relativeToGate());
+    enemyTypesPlaces.push(turnDirection());
+    for (var i = 2; i < count; i++) {
         let shipType = (Math.floor(Math.random() * 2)) > 0 ? ShipType.CRUSER : ShipType.CORVETTE;
         // get random X between 200 and width of screen - 200
         let x = width - 200 - (i * 200);
@@ -71,6 +73,29 @@ function randomEnemies() {
     return enemyTypesPlaces;
 }
 
+function relativeToGate() {
+    let gatePlace = { "x": (getWidth() - 100), "y": ((getHeight() - 75) / 2) };
+    let start = getPointRelativeTo(-300, -100, gatePlace);
+    let second = getPointRelativeTo(-200, -200, gatePlace);
+    let third = getPointRelativeTo(-400, 200, gatePlace);
+    return {
+        "x": start.x, "y": start.y, "shipType": ShipType.CRUSER, "route":
+            [
+                { "x": second.x, "y": second.y },
+                { "x": third.x, "y": third.y },
+                { "x": start.x, "y": start.y }
+            ]
+    };
+}
+function turnDirection() {
+    let x = getWidth() - 600;
+    let y = ((getHeight() - 75) / 2);
+    return {
+        "x": x, "y": y, "shipType": ShipType.CRUSER, "route":
+            getTurnRoute({ "x": x, "y": y }, (3 * Math.PI) / 4, ShipType.CRUSER)
+    };
+}
+
 function getWidth() {
     return Math.max(
         document.body.scrollWidth,
@@ -91,7 +116,7 @@ function getHeight() {
     );
 }
 
-// convert negative radians to positive
+// convert negative radians to positive, or over 2pi to within 0-2pi
 function rad2Pos(rad) {
     if (rad < 0) {
         return rad2Pos(rad + 2 * Math.PI);
@@ -100,4 +125,23 @@ function rad2Pos(rad) {
         return rad2Pos(rad - 2 * Math.PI);
     }
     return rad;
+}
+// gets a point relative to other object
+function getPointRelativeTo(distX, distY, point) {
+    if (!("x" in point) || !("y" in point)) {
+        throw Error("Invalid point, needs 'x' and 'y'.");
+    }
+    return { "x": (distX + point.x), "y": (distY + point.y) };
+}
+// build route from point with turn of turnRad left & right for shipType
+function getTurnRoute(point, turnRad, shipType) {
+    let radius = ((shipType === ShipType.CORVETTE) ? 10 : 5) / 2;
+    // get center of ship for turn, assuming default direction of 0 rad
+    let cX = point.x - radius;
+    let cY = point.y;
+    let x1 = cX + (radius * Math.cos(turnRad));
+    let y1 = cY + (radius * Math.sin(turnRad));
+    let x2 = cX + (radius * Math.cos(2 * turnRad));
+    let y2 = cY + (radius * Math.sin(2 * turnRad));
+    return [{ "x": x1, "y": y1 }, { "x": x2, "y": y2 }];
 }
